fd9c8e16cb46370949a20c86099e0d42df85c22b
Ariel Brassesco <ariel.brassesco@gmail.com>
2024-10-17 00:03:41 -0300

    fix(commit): fix commitizen and build warnings

--- dist/index.d.mts
+++ dist/index.d.mts
@@ -1,11 +1,33 @@
 import * as paseto from 'paseto';
 import { ProduceOptions, ConsumeOptions } from 'paseto';
 
-type AssymetricKey = "paserk";
+type AsymetricKey = "paserk";
 type PasetoTokenContext = "public";
 declare class PasetoV4Handler {
-    generateKey(purpose: PasetoTokenContext, options: {
-        format: AssymetricKey;
+    /**
+     * Generate asymetric public/private key
+     *
+     * @param purpose - Only support "public"
+     * @param options - Only support { format: "paserk" }
+     *
+     * @returns
+     * `{ secretKey: string; publicKey: string; }`
+     *
+     *
+     * @example
+     * ```ts
+     * PasetoV4Handler.generateKey("public", { format: "paserk" });
+     *
+     * returns
+     * {
+     *   secretKey: 'k4.secret.ICXpik-1FLvjjvspZuFpzudF2WMkjsmBECOkjlNR6lb7C_pYpZkYoB6wW0RTVQTSKNO2kYrL55lxnctEY6fabw',
+     *   publicKey: 'k4.public.-wv6WKWZGKAesFtEU1UE0ijTtpGKy-eZcZ3LRGOn2m8'
+     * }
+     *
+     * ```
+     */
+    static generateKey(purpose: PasetoTokenContext, options: {
+        format: AsymetricKey;
     }): Promise<{
         secretKey: string;
         publicKey: string;
@@ -16,6 +38,37 @@ declare class PasetoV4Handler {
         purpose: "local" | "public";
         version: string;
     };
+    /**
+     *
+     * @remarks
+     * Claims reserved
+     *
+     * | Key |    Name    |  Type  |             Example                 |
+     *
+     * | iss |   Issuer   | string |       {"iss":"paragonie.com"}       |
+     *
+     * | sub |  Subject   | string |            {"sub":"test"}           |
+     *
+     * | aud |  Audience  | string |       {"aud":"pie-hosted.com"}      |
+     *
+     * | exp | Expiration | DtTime | {"exp":"2039-01-01T00:00:00+00:00"} |
+     *
+     * | nbf | Not Before | DtTime | {"nbf":"2038-04-01T00:00:00+00:00"} |
+     *
+     * | iat | Issued At  | DtTime | {"iat":"2038-03-17T00:00:00+00:00"} |
+     *
+     * | jti |  Token ID  | string |  {"jti":"87IFSGFgPNtQNNuw0AtuLttP"} |
+     *
+     * | kid |   Key-ID   | string |    {"kid":"stored-in-the-footer"}   |
+     *
+     *
+     *
+     * @param payload - a Record with data to sign
+     * @param privateKey - private key as string
+     * @param options - options for paseto v4 algorimth
+     * @returns
+     * Paseto V4 public token format.
+     */
     sign(payload: Record<string, any>, privateKey: string, options?: ProduceOptions): Promise<string>;
     verify(token: string, publicKey: string, options?: ConsumeOptions<true>): Promise<paseto.CompleteResult<Record<string, unknown>>>;
 }
--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
@@ -27,33 +28,83 @@ module.exports = __toCommonJS(src_exports);
 // src/utils/paseto.ts
 var import_paseto = require("paseto");
 var PasetoV4Handler = class {
+  static {
+    __name(this, "PasetoV4Handler");
+  }
   /*
-  Claims reserved
-  +-----+------------+--------+-------------------------------------+
-  | Key |    Name    |  Type  |               Example               |
-  +-----+------------+--------+-------------------------------------+
-  | iss |   Issuer   | string |       {"iss":"paragonie.com"}       |
-  | sub |  Subject   | string |            {"sub":"test"}           |
-  | aud |  Audience  | string |       {"aud":"pie-hosted.com"}      |
-  | exp | Expiration | DtTime | {"exp":"2039-01-01T00:00:00+00:00"} |
-  | nbf | Not Before | DtTime | {"nbf":"2038-04-01T00:00:00+00:00"} |
-  | iat | Issued At  | DtTime | {"iat":"2038-03-17T00:00:00+00:00"} |
-  | jti |  Token ID  | string |  {"jti":"87IFSGFgPNtQNNuw0AtuLttP"} |
-  | kid |   Key-ID   | string |    {"kid":"stored-in-the-footer"}   |
-  +-----+------------+--------+-------------------------------------+
+  
   */
-  generateKey(purpose, options) {
+  /**
+  * Generate asymetric public/private key
+  *
+  * @param purpose - Only support "public"
+  * @param options - Only support { format: "paserk" }
+  *
+  * @returns
+  * `{ secretKey: string; publicKey: string; }`
+  *
+  *
+  * @example
+  * ```ts
+  * PasetoV4Handler.generateKey("public", { format: "paserk" });
+  *
+  * returns
+  * {
+  *   secretKey: 'k4.secret.ICXpik-1FLvjjvspZuFpzudF2WMkjsmBECOkjlNR6lb7C_pYpZkYoB6wW0RTVQTSKNO2kYrL55lxnctEY6fabw',
+  *   publicKey: 'k4.public.-wv6WKWZGKAesFtEU1UE0ijTtpGKy-eZcZ3LRGOn2m8'
+  * }
+  *
+  * ```
+  */
+  static async generateKey(purpose, options) {
     return import_paseto.V4.generateKey(purpose, options);
   }
   decode(token) {
     const data = (0, import_paseto.decode)(token);
     try {
       const footer = JSON.parse(data.footer?.toString() ?? "");
-      return { ...data, footer };
+      return {
+        ...data,
+        footer
+      };
     } catch {
     }
-    return { ...data, footer: data.footer?.toString() };
+    return {
+      ...data,
+      footer: data.footer?.toString()
+    };
   }
+  /**
+  *
+  * @remarks
+  * Claims reserved
+  *
+  * | Key |    Name    |  Type  |             Example                 |
+  *
+  * | iss |   Issuer   | string |       {"iss":"paragonie.com"}       |
+  *
+  * | sub |  Subject   | string |            {"sub":"test"}           |
+  *
+  * | aud |  Audience  | string |       {"aud":"pie-hosted.com"}      |
+  *
+  * | exp | Expiration | DtTime | {"exp":"2039-01-01T00:00:00+00:00"} |
+  *
+  * | nbf | Not Before | DtTime | {"nbf":"2038-04-01T00:00:00+00:00"} |
+  *
+  * | iat | Issued At  | DtTime | {"iat":"2038-03-17T00:00:00+00:00"} |
+  *
+  * | jti |  Token ID  | string |  {"jti":"87IFSGFgPNtQNNuw0AtuLttP"} |
+  *
+  * | kid |   Key-ID   | string |    {"kid":"stored-in-the-footer"}   |
+  *
+  *
+  *
+  * @param payload - a Record with data to sign
+  * @param privateKey - private key as string
+  * @param options - options for paseto v4 algorimth
+  * @returns
+  * Paseto V4 public token format.
+  */
   async sign(payload, privateKey, options) {
     return import_paseto.V4.sign(payload, privateKey, options);
   }
